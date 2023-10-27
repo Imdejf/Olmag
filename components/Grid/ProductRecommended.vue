fa41f34b7356deb552d7b12d530e488b9eedb957
Dawid Jabłoński <jablonskidawid0202@gmail.com>
2023-10-27 20:09:01 +0200

    Added product type

--- components/Grid/ProductRecommended.vue
+++ components/Grid/ProductRecommended.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { ProductAvailability } from "~/types/Product/productTypes";
+import { ProductAvailability } from "~/types/Product/productTypes.ts";
 
 const props = defineProps({
   products: {
